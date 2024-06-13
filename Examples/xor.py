97b53498502189ca7b6de18d9598bffa85652c0e
Pranav Chandane <pranavhchandane@gmail.com>
2024-06-13 20:37:13 +0530

    changed activation function to Tanh from Sigmoid. Tensor init uses requires_grad instead of autograd since the field is renamed.

--- Examples/xor.py
+++ Examples/xor.py
@@ -1,7 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-from nn.activations import Sigmoid
+from nn.activations import Tanh
 from nn.layers import Sequential, Dense
 from nn.losses import MSE
 from nn.optim import Adam
@@ -11,13 +11,13 @@ from nn.utils import DataLoader, save_object, load_object
 X_train = np.reshape([[0,0], [0, 1], [1, 0], [1, 1]], (4, 2))
 y_train = np.reshape([[0], [1], [1], [0]], (4, 1))
 
-data = DataLoader(X_train, y_train, batch_size=4, shuffle=True, autograd=True)
+data = DataLoader(X_train, y_train, batch_size=4, shuffle=True, requires_grad=True)
 
 model = Sequential([
     Dense(2, 24),
-    Sigmoid(),
+    Tanh(),
     Dense(24, 4),
-    Sigmoid(),
+    Tanh(),
     Dense(4, 1),
 ])
 
@@ -62,5 +62,5 @@ decision_boundary()
 
 # Predictions
 print("\nInference:")
-pred:Tensor = loaded_model.forward(Tensor(X_train, autograd=False))
+pred:Tensor = loaded_model.forward(Tensor(X_train, requires_grad=False))
 for y_hat, y_train in zip(pred.data[:,0], y_train[:,0]): print(f"Prediction: {y_hat:.4f} - Actual: {y_train}")
